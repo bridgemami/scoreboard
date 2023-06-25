adb14e22aab6ef9b04fb660cba6ed5b02c6845cc
Michael <mdbridgeman@gmail.com>
2023-06-25 15:49:53 -0700

    added onclick attribute onto point buttons

--- script.js
+++ script.js
@@ -8,59 +8,16 @@ const awayScoreboard = document.querySelector("#awayScore");
 let awayScore = 0;
 awayScoreboard.textContent = awayScore;
 
-// Home score buttons
-const homeOne = document.querySelector("#plusOneHome");
-const homeTwo = document.querySelector("#plusTwoHome");
-const homeThree = document.querySelector("#plusThreeHome");
-const homeSix = document.querySelector("#plusSixHome");
-
-// Away score buttons
-const awayOne = document.querySelector("#plusOneAway");
-const awayTwo = document.querySelector("#plusTwoAway");
-const awayThree = document.querySelector("#plusThreeAway");
-const awaySix = document.querySelector("#plusSixAway");
-
-//event listeners for the home team
-homeOne.addEventListener("click", () => {
-  homeScore += 1;
+//add the home points
+const addPointsToHomeTeam= (points) => {
+  homeScore += points;
   homeScoreboard.textContent = homeScore;
-});
-
-homeTwo.addEventListener("click", () => {
-  homeScore += 2;
-  homeScoreboard.textContent = homeScore;
-});
-
-homeThree.addEventListener("click", () => {
-  homeScore += 3;
-  homeScoreboard.textContent = homeScore;
-});
-
-homeSix.addEventListener("click", () => {
-  homeScore += 6;
-  homeScoreboard.textContent = homeScore;
-});
-
-//event listeners for the away team
-awayOne.addEventListener("click", () => {
-  awayScore += 1;
+}
+//add the away points
+const addPointsToAwayTeam = (points) => {
+  awayScore += points;
   awayScoreboard.textContent = awayScore;
-});
-
-awayTwo.addEventListener("click", () => {
-  awayScore += 2;
-  awayScoreboard.textContent = awayScore;
-});
-
-awayThree.addEventListener("click", () => {
-  awayScore += 3;
-  awayScoreboard.textContent = awayScore;
-});
-
-awaySix.addEventListener("click", () => {
-  awayScore += 6;
-  awayScoreboard.textContent = awayScore;
-});
+}
 
 //reset the score
 const reset = document.querySelector("#reset");
